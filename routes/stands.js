8c622236166d1dbf7460e2b9ae21b3e134781a7a
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-07-12 20:07:05 -0600

    Sync changes

--- routes/stands.js
+++ routes/stands.js
@@ -1,29 +1,29 @@
 /***************************************
-* Stand routes
-* 
-* all routes in this file begin with /stands 
-*/
+ * Stand routes
+ *
+ * all routes in this file begin with /stands
+ */
 
 // includes ...
-const router = require( 'express' ).Router();
-const standsController = require( '../controllers/stands' );
+const router = require('express').Router();
+const standsController = require('../controllers/stands');
 // const { auth, requiresAuth } = require( 'express-openid-connect'); // dereference return
 
 ////////////////////////////////////////////////////////////////
 
 // GET stand that matches authid else redirect to setup page
-router.get( '/', standsController.getStand );
+router.get('/', standsController.getStand);
 
 // GET a specific stand
-router.get( '/:id', standsController.getStand );
+router.get('/:id', standsController.getStand);
 
 // POST create a new stand (and user)
 //router.post( '/name/:name', standsController.createStand );
-router.post( '/', standsController.createStand );
+router.post('/', standsController.createStand);
 // router.post( '/name/:name', requireAuth(), standsController.createStand );
 
 // TODO: PUT update stand (day and earnings)
-router.post( '/put/:id', standsController.updateStand );
+router.post('/put/:id', standsController.updateStand);
 //router.post( '/op/:op/brains/:brains', standsController.updateStand );
 
 ////////////////////////////////////////////////////////////////
